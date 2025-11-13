0f45ceb3ee3a1c94aa9cd2de127ec001017aaa58
Aidan Brown <74436589+NotAidanBrown@users.noreply.github.com>
2025-11-13 15:38:52 -0600

    Delete board directory

--- board/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('board/', views.board, name='board'),
-    path('board/api/upload_card_image/', views.upload_card_image, name='upload_card_image'),
-]
