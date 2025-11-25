48e3871d0afd7821bdbc213a4fd4e10cb1a44b7f
matt4205 <mjmattjones42@gmail.com>
2025-11-25 14:05:49 -0600

    Delete pls work directory

--- Virtual-Table-Top-main/board/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import CardImage
-
-
-@admin.register(CardImage)
-class CardImageAdmin(admin.ModelAdmin):
-	list_display = ('id', 'uploaded_by', 'uploaded_at')
-	readonly_fields = ('uploaded_at',)
