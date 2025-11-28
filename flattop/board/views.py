2da02988786cab9760f6b6048e6497a397804fb5
Matt Jones <mjmattjones42@gmail.com>
2025-11-28 00:37:20 -0600

    Current everything working version

--- flattop/board/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.template import loader
-
-def board(request):
-    template = loader.get_template('board/layout.html')
-    return HttpResponse(template.render())
-# Create your views here.
--- flattop/urls.py
+++ flattop/urls.py
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-from django.contrib import admin
-from django.urls import include, path
-from django.views.generic import RedirectView
-from django.conf import settings
-from django.conf.urls.static import static
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', RedirectView.as_view(url='/accounts/login/', permanent=False)),
-    path('', include('board.urls'))
-
-]
-
-if settings.DEBUG:
-    # serve media files in development
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-=======
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
@@ -46,4 +26,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> matt
