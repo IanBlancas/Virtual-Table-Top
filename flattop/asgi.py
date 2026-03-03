6833c043743d7fea9f1ab8a9c5cd5bf04a042a01
Matt Jones <matt@scalybones.com>
2026-03-02 20:20:39 -0600

    Fixed css problem

--- flattop/asgi.py
+++ flattop/asgi.py
@@ -1,5 +1,6 @@
 import os
 from django.core.asgi import get_asgi_application
+from django.contrib.staticfiles.handlers import ASGIStaticFilesHandler
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import flattop.routing
@@ -9,7 +10,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flattop.settings")
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": ASGIStaticFilesHandler(django_asgi_app),
     "websocket": AuthMiddlewareStack(
         URLRouter(flattop.routing.websocket_urlpatterns)
     ),
